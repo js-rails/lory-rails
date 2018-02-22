9fa7ed81049a27813683b7022155d130ebec9222
tanvir hasan <tanvir002700@gmail.com>
2018-02-22 22:21:38 +0600

    remove bin and spec dir

--- spec/lory/rails_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe Lory::Rails do
-  it "has a version number" do
-    expect(Lory::Rails::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
