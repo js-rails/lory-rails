b291c98e960758d02831893ce004685f10869b05
tanvir hasan <tanvir002700@gmail.com>
2018-02-22 22:18:11 +0600

    first commit

---
+++ lib/lory/rails.rb
@@ -0,0 +1,7 @@
+require "lory/rails/version"
+
+module Lory
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ spec/lory/rails_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Lory::Rails do
+  it "has a version number" do
+    expect(Lory::Rails::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
