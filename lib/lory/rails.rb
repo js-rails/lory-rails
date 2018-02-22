35edc81e79053a3d13c7d85279b93d35ab21d08d
tanvir hasan <tanvir002700@gmail.com>
2018-02-22 22:35:31 +0600

    override rails engine

--- lib/lory/rails.rb
+++ lib/lory/rails.rb
@@ -1,7 +1,2 @@
 require "lory/rails/version"
-
-module Lory
-  module Rails
-    # Your code goes here...
-  end
-end
+require "lory/rails/engine" if defined? Rails
---
+++ lib/lory/rails/engine.rb
@@ -0,0 +1,6 @@
+module Lory
+  module Rails
+    class Engine < ::Rails::Engine; end
+  end
+end
+
